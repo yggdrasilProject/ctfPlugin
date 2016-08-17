293083385467e409163099c425aa99a7ce686339
Alexey Deryugin <aderyugin@mirantis.com>
2016-08-17 14:45:28 +0300

    Initial commit
    Change-Id: Ie4c658c0db859e2a226ab45f2d3882a2ce6cffed

---
+++ src/main/java/ru/linachan/ctf/CTFPlugin.java
@@ -0,0 +1,16 @@
+package ru.linachan.ctf;
+
+import ru.linachan.yggdrasil.plugin.YggdrasilPlugin;
+
+public class CTFPlugin implements YggdrasilPlugin {
+
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onShutdown() {
+
+    }
+}
