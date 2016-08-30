dea952c9a5703d82c20ac67f93e53f043078b9b1
Alexey Deryugin <aderyugin@mirantis.com>
2016-08-30 19:49:53 +0300

    Add Web Queue Status interface
     - Add ability to monitor queue status via HTTP
    
    Change-Id: Ia926f1a22bf6ce81f920247af3e52d08fd145bf7

--- src/main/java/ru/linachan/ctf/CTFHTTPFlagListener.java
+++ src/main/java/ru/linachan/ctf/CTFHTTPFlagListener.java
@@ -1,8 +1,13 @@
 package ru.linachan.ctf;
 
+import com.mongodb.client.MongoCollection;
 import com.sun.net.httpserver.*;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.bson.Document;
+import org.json.simple.JSONObject;
 import ru.linachan.yggdrasil.YggdrasilCore;
 import ru.linachan.yggdrasil.common.Queue;
+import ru.linachan.yggdrasil.plugin.YggdrasilPluginManager;
 import ru.linachan.yggdrasil.service.YggdrasilService;
 
 import java.io.IOException;
@@ -10,12 +15,13 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.io.IOUtils;
 
 public class CTFHTTPFlagListener implements HttpHandler, YggdrasilService {
 
     private HttpServer flagServer;
     private static final Pattern REQUEST_PATTERN = Pattern.compile(
-        "^/flag/(?<type>hi|no|lo)/(?<flag>[a-fA-F0-9]{32}=)$"
+        "^/flag/((?<type>hi|no|lo)/(?<flag>[a-fA-F0-9]{32}=)|(?<static>status|status\\.json))$"
     );
 
     private Queue<String> flagQueue;
@@ -50,32 +56,81 @@ public class CTFHTTPFlagListener implements HttpHandler, YggdrasilService {
     public void run() { }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void handle(HttpExchange exchange) throws IOException {
         Matcher matcher = REQUEST_PATTERN.matcher(exchange.getRequestURI().toString());
+        OutputStream os;
+
         if (matcher.matches()) {
-            byte[] bytes = "OK".getBytes();
-            exchange.sendResponseHeaders(200, bytes.length);
+            if (matcher.group("type") != null) {
+                switch (matcher.group("type")) {
+                    case "hi":
+                        flagQueue.push(String.format("%s:%d", matcher.group("flag"), 2));
+                        break;
+                    case "no":
+                        flagQueue.push(String.format("%s:%d", matcher.group("flag"), 1));
+                        break;
+                    case "lo":
+                        flagQueue.push(String.format("%s:%d", matcher.group("flag"), 0));
+                        break;
+                }
 
-            switch (matcher.group("type")) {
-                case "hi":
-                    flagQueue.push(String.format("%s:%d", matcher.group("flag"), 2));
-                    break;
-                case "no":
-                    flagQueue.push(String.format("%s:%d", matcher.group("flag"), 1));
-                    break;
-                case "lo":
-                    flagQueue.push(String.format("%s:%d", matcher.group("flag"), 0));
-                    break;
+                byte[] bytes = "OK".getBytes();
+                exchange.sendResponseHeaders(200, bytes.length);
+
+                os = exchange.getResponseBody();
+                os.write(bytes);
+                os.close();
+            } else if (matcher.group("static") != null) {
+                MongoCollection<Document> flags = YggdrasilCore.INSTANCE
+                    .getManager(YggdrasilPluginManager.class).get(CTFPlugin.class)
+                    .getDB().getCollection("flags");
+
+                switch (matcher.group("static")) {
+                    case "status":
+                        ByteArrayOutputStream template = new ByteArrayOutputStream();
+                        IOUtils.copy(
+                            this.getClass().getResourceAsStream("/status.html"),
+                            template
+                        );
+
+                        exchange.getResponseHeaders().add("Content-Type", "text/html");
+                        exchange.sendResponseHeaders(200, template.size());
+
+                        os = exchange.getResponseBody();
+                        os.write(template.toByteArray());
+                        os.close();
+                        break;
+                    case "status.json":
+                        JSONObject stats = new JSONObject();
+                        JSONObject queued = new JSONObject();
+
+                        queued.put("high", flags.count(new Document("state", 0).append("priority", 2)));
+                        queued.put("normal", flags.count(new Document("state", 0).append("priority", 1)));
+                        queued.put("low", flags.count(new Document("state", 0).append("priority", 0)));
+
+                        stats.put("processing", flagQueue.list().size());
+                        stats.put("queued", queued);
+                        stats.put("sent", flags.count(new Document("state", 1)));
+                        stats.put("invalid", flags.count(new Document("state", 2)));
+                        stats.put("total", flags.count());
+
+                        byte[] statsBytes = stats.toJSONString().getBytes();
+
+                        exchange.getResponseHeaders().add("Content-Type", "application/json");
+                        exchange.sendResponseHeaders(200, statsBytes.length);
+
+                        os = exchange.getResponseBody();
+                        os.write(statsBytes);
+                        os.close();
+                        break;
+                }
             }
-
-            OutputStream os = exchange.getResponseBody();
-            os.write(bytes);
-            os.close();
         } else {
             byte[] bytes = "BADASS".getBytes();
             exchange.sendResponseHeaders(400, bytes.length);
 
-            OutputStream os = exchange.getResponseBody();
+            os = exchange.getResponseBody();
             os.write(bytes);
             os.close();
         }
